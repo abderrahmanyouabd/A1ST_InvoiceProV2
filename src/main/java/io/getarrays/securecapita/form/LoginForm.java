83aeaa932ed918b4255036660eb7b581b01e8fc3
Abderrahman Youabd <youabd2000@gmail.com>
2024-01-07 15:41:15 +0100

    Initial commit V2

---
+++ src/main/java/io/getarrays/securecapita/form/LoginForm.java
@@ -0,0 +1,21 @@
+package io.getarrays.securecapita.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+/**
+ * @author Junior RT
+ * @version 1.0
+ * @license Get Arrays, LLC (https://getarrays.io)
+ * @since 12/25/2022
+ */
+
+@Data
+public class LoginForm {
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Invalid email. Please enter a valid email address")
+    private String email;
+    @NotEmpty(message = "Password cannot be empty")
+    private String password;
+}
