bcaa4ef158f00d0219b217e6b5c473d826617440
Abderrahman Youabd <youabd2000@gmail.com>
2024-01-08 17:56:56 +0100

    sms implemented with twilio and service tested success.

--- src/main/java/com/a1st/invoicepro/utils/SmsUtils.java
+++ src/main/java/com/a1st/invoicepro/utils/SmsUtils.java
@@ -11,9 +11,9 @@ import static com.twilio.rest.api.v2010.account.Message.creator;
  * @version: 1.0
  */
 public class SmsUtils {
-    public static final String FROM_NUMBER = "<Your own number from Twilio>";
-    public static final String SID_KEY = "<Your own key>";
-    public static final String TOKEN_KEY = "<Your own key>";
+    public static final String FROM_NUMBER = "+447883285927";
+    public static final String SID_KEY = "AC0827014313f3054c5b8b736e5fa84332";
+    public static final String TOKEN_KEY = "77325a89f3d47b4fa11aaed4caee68ef";
 
     public static void sendSMS(String to, String messageBody) {
         Twilio.init(SID_KEY, TOKEN_KEY);
